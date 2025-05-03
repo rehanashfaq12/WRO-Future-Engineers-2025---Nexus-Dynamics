73a2e76965fec93acfbc69c42972830c4d6f463b
ShadowRaider12 <kdakowuah@gmail.com>
2025-05-03 13:22:06 +0000

    robot and cam config

--- WRO Future Engineers 2025/obstaclechallenge.py
+++ WRO Future Engineers 2025/obstaclechallenge.py
@@ -3,3 +3,8 @@ import numpy as np
 import time
 from gpiozero import CamJamKitRobot
 from simple_pid import PID
+
+robot = CamJamKitRobot()
+camera = cv2.VideoCapture(0)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
